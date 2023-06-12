4f750b34b00d2f4124378920022d8e84e90902ec
mostafizur <mostafizurm01@gmail.com>
2023-06-12 21:39:30 +0600

    what we do section added

--- src/Pages/Home/WhatWeDo/SingleWhatWeDo.jsx
+++ src/Pages/Home/WhatWeDo/SingleWhatWeDo.jsx
@@ -8,10 +8,10 @@ const SingleWhatWeDo = ({ item }) => {
       <div   className=" rounded-md p-8 border-2  hover:bg-[#6772e5] transition duration-500 ease-in-out transform">
         <div className="hover:bg-[#6772e5]">
           <div className="card mt-8 hover:bg-[#6772e5] ">
-            <img className=" rounded-full w-20 mx-auto" src={image} alt="" />
+            <img className=" rounded-full w-20 mx-auto" src={image} alt=""/>
             <div className="text-center space-y-2 mt-4">
               <h2 className="text-xl">{name}</h2>
-              <p> {description}</p>
+              <p> {description}</p> 
             </div>
           </div>
         </div>
